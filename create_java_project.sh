85b61487cf7cd0b40f70e8668c8b8bfe8e3291bd
Raja. S <rajasoun@cisco.com>
2012-02-21 12:34:55 +0530

    Check Script Arguments

--- create_java_project.sh
+++ create_java_project.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env sh
 ARTIFACT_ID=$1
 
+#Check the Program Arguments are Present
+if [ $# -lt 1 ]; then
+echo 1>&2 Usage: $0 ApplicationName
+         exit 1
+fi
+
+#Check Program Argument is not Empty
+if [ "$ARTIFACT_ID" == "" ]; then
+echo 1>&2 Usage: $0 ApplicationName
+         exit 1
+fi
+
 mvn archetype:create -DgroupId=com.cisco.eaas.spike -DartifactId=$1 -Dpackagename=com.cisco.eaas.spike
