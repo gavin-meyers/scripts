9084c494c00fec3d1a87bcd3801b752132cc1079
Raja. S <rajasoun@cisco.com>
2012-08-14 17:56:29 +0530

    Added Cucumber watir Scripts
    Test Automation

---
+++ clh-automation/references/cucumber-watir-example/features/step_definitions/weather_steps.rb
@@ -0,0 +1,24 @@
+require 'spec/expectations'
+require 'watir'
+
+require 'safariwatir'
+require 'rubygems'
+
+Given /^I am on the weather\.com homepage$/ do
+  # @browser = Watir::Safari.new
+  @browser = Watir::Browser.new
+  @browser.goto('http://www.weather.com')
+end
+
+When /^I search for the zip code "([^"]*)"$/ do |zipcode|
+  @browser.text_field(:id, 'typeaheadBox').set(zipcode)
+  @browser.button(:id, 'twc-weather-search-submit-id').click
+end
+
+Then /^I should see "([^"]*)"$/ do |what_i_should_see|
+  @browser.text.should include(what_i_should_see)
+end
+
+Then /^I should not see "([^"]*)"$/ do |what_i_should_not_see|
+  @browser.text.should_not include(what_i_should_not_see)
+end
